cc815a32a0c0c400982bc11f9b627b1a60b94008
biersoeckli <meow@biersoeckli.ch>
2023-12-01 12:34:37 +0000

    added frontend role management

--- attendance-list/src/app/public-profile/public-profile.component.ts
+++ attendance-list/src/app/public-profile/public-profile.component.ts
@@ -9,7 +9,6 @@ import { DepartmentService } from '../services/department.service';
 import Swal from 'sweetalert2';
 import { StatusService } from '../services/status.service';
 import { GeolocationService } from '../services/geo-location.service';
-import { DomSanitizer } from '@angular/platform-browser';
 import * as L from 'leaflet';
 @Component({
   selector: 'app-public-profile',
@@ -29,6 +28,7 @@ export class PublicProfileComponent implements OnInit {
 
   @ViewChild('lMap') mapElRef: ElementRef;
   hideLocation: boolean;
+  userRoles?: string[];
 
   constructor(private route: ActivatedRoute,
     private snackbar: MatSnackBar,
@@ -62,6 +62,8 @@ export class PublicProfileComponent implements OnInit {
             this.userService.isLoggedInUserInRole(UserRoles.admin).then(res => this.isAdmin = res);
             this.profileIsfromCurrentUser = currentLoggedInUser.id === this.currentUserStatus.user.id;
             this.stv = this.userService.getStellvertreterUserStatus(this.currentUserStatus.user);
+
+            this.userRoles = await Parse.Cloud.run('getRolesFromUser', { userId: this.currentUserStatus.user.id });
 
             await this.initLocationMap();
           }
@@ -119,30 +121,6 @@ export class PublicProfileComponent implements OnInit {
     map.addLayer(layer);
   }
 
-  async deleteUser() {
-    Swal.fire({
-      title: 'Willst du diesen Benutzer wirklich löschen?',
-      showCancelButton: true,
-      confirmButtonText: 'Ja',
-      cancelButtonText: 'Nein',
-      showLoaderOnConfirm: true
-    }).then((result) => {
-      if (result.value) {
-        Parse.Cloud.run('deleteUser', { user2delete: this.currentUserStatus.user.id }).then(result2 => {
-
-          this.snackbar.open('Benutzer wurde gelöscht.', null, {
-            duration: 4000
-          });
-          this.router.navigateByUrl('/minions');
-        }).catch(err => {
-          this.snackbar.open('Es ist ein Fehler aufgetreten', null, {
-            duration: 3000
-          });
-        });
-      }
-    });
-
-  }
 
   async toggleStatus() {
 
@@ -174,6 +152,93 @@ export class PublicProfileComponent implements OnInit {
     }
   }
 
+
+  async removeRole(roleName: string) {
+    Swal.fire({
+      title: 'Willst du die Rolle wirklich entfernen?',
+      showCancelButton: true,
+      confirmButtonText: 'Ja',
+      cancelButtonText: 'Nein',
+      showLoaderOnConfirm: true
+    }).then((result) => {
+      if (result.value) {
+        Parse.Cloud.run('removeRoleFromUser', { roleName, userId: this.currentUserStatus.user.id }).then(async result2 => {
+
+          this.snackbar.open('Rolle wurde entfernt.', null, {
+            duration: 4000
+          });
+          this.userRoles = await Parse.Cloud.run('getRolesFromUser', { userId: this.currentUserStatus.user.id });
+
+        }).catch(err => {
+          this.snackbar.open('Es ist ein Fehler aufgetreten', null, {
+            duration: 3000
+          });
+        });
+      }
+    });
+
+  }
+
+  async deleteUser() {
+    Swal.fire({
+      title: 'Willst du diesen Benutzer wirklich löschen?',
+      showCancelButton: true,
+      confirmButtonText: 'Ja',
+      cancelButtonText: 'Nein',
+      showLoaderOnConfirm: true
+    }).then((result) => {
+      if (result.value) {
+        Parse.Cloud.run('deleteUser', { user2delete: this.currentUserStatus.user.id }).then(result2 => {
+
+          this.snackbar.open('Benutzer wurde gelöscht.', null, {
+            duration: 4000
+          });
+          this.router.navigateByUrl('/minions');
+        }).catch(err => {
+          this.snackbar.open('Es ist ein Fehler aufgetreten', null, {
+            duration: 3000
+          });
+        });
+      }
+    });
+
+  }
+
+  async addRoleToUser() {
+
+
+    const query = new Parse.Query('_Role');
+    const roles = await query.find();
+
+    const roleNames = roles.map(x => x.get('name'));
+    const filteredRoles = roleNames.filter(x => !this.userRoles.includes(x));
+    const inputOptionsMap = new Map();
+    filteredRoles.forEach(x => inputOptionsMap.set(x, x));
+
+    Swal.fire({
+      title: 'Rolle hinzufügen',
+      text: 'Wählen Sie eine Rolle:',
+      input: 'select',
+      inputOptions: inputOptionsMap,
+      showCancelButton: true,
+      confirmButtonText: 'Hinzufügen',
+      cancelButtonText: 'Abbrechen',
+      showLoaderOnConfirm: true
+    }).then((result) => {
+      if (result.value) {
+        Parse.Cloud.run('addUserToRole', { roleName: result.value, userId: this.currentUserStatus.user.id }).then(async result2 => {
+          this.snackbar.open('Rolle "' + result.value + '" wurde hinzugefügt', null, {
+            duration: 2000
+          });
+          this.userRoles = await Parse.Cloud.run('getRolesFromUser', { userId: this.currentUserStatus.user.id });
+        }).catch(err => {
+          this.snackbar.open('Es ist ein Fehler aufgetreten', null, {
+            duration: 3000
+          });
+        });
+      }
+    });
+  }
 
   async changePassword() {
     Swal.fire({
